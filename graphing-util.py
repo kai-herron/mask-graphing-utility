a8b1b14ad28587eb87d25681bdfd65ddaf29c392
Kai Herron <kherron@des81.fnal.gov>
2023-06-22 11:54:35 -0500

    Update code to scatter plot RAs and Decs using skyproj

--- graphing-util.py
+++ graphing-util.py
@@ -33,43 +33,25 @@ def graph_failed_objects(fore_mask,ra,dec,tilename,nested):
     hp.write_map(STOR_DIR,mask,overwrite=True,nest=False)
     hspmap = hsp.HealSparseMap.read(STOR_DIR,
                                    nside_coverage=nside)
-    
-    #Get our foreground array
-    fig = plt.figure(figsize=(8,8))
-    ax = fig.add_subplot(111)
-    sp = skyproj.Skyproj(ax=ax)
-    im,ras,decs,val = sp.draw_hspmap(hspmap,cmap='jet',alpha=0.8)
-    #plt.show()
-    plt.clf()
-    
-    #Get our 2D Hist array
-    _ = plt.figure(figsize=(8,8))
-    sel = (ras > min(ra)) & (ras < max(ra)) & (decs > min(dec)) & (decs < max(dec))
+    #Get rid of objects that are masked
+    mask = hp.read_map(fore_mask,nest=nested)
     pix = []
     for i in range(len(ra)):
         pix = np.append(pix,hp.ang2pix(nside,ra[i],dec[i],lonlat=True))
-        
-    mask = hp.read_map(fore_mask,nest=nested)
+    
     
     indices = []
     for i in range(len(pix)):
         indices = np.append(indices, mask[int(pix[i])]==0)
     indices=indices.astype(bool)
-    fig3 = plt.hist2d(ra[indices],dec[indices],bins=np.shape(val)[0],norm=LogNorm(),cmap='jet')
-    plt.gca().invert_xaxis()
-    #plt.show()
-    plt.clf()
+    #Get our foreground array
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_subplot(111)
+    sp = skyproj.Skyproj(ax=ax)
+    im,ras,decs,val = sp.draw_hspmap(hspmap,cmap='jet',alpha=0.8)
+    sp.scatter(ra[indices],dec[indices],s=2,color='magenta')
     
-    #Finally plot it all together
-    fig = plt.figure(figsize=(10,10))
     
-    plt.title(tilename)
+        
     
-    sel = fig3[0] != 0
-    ma = np.ma.MaskedArray(data=fig3[0],mask =sel )    
-    plt.imshow(np.flip(np.rot90(ma),axis=1),cmap='jet')
-    plt.imshow(np.flip(np.flip(val,axis=1),axis=0),alpha=0.7,cmap='jet',norm=LogNorm())
-    
-    plt.xticks(ticks=[])
-    plt.yticks(ticks=[])
     plt.show()
